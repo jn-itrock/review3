e22224a43d6eaeb56a09cccf82b0072a6ba7e24e
Joaco <joaquin.naftaly@itrocksweb.com>
2023-08-17 14:50:26 -0300

    metadata

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -28,6 +28,7 @@ export class EventController {
     @Body() data: CreateEventDto,
     @UploadedFile() file: Express.Multer.File
   ): Promise<Document<Event>> {
+    console.log("llega");
     return await this.eventService.create(data, file);
   }
 
@@ -38,8 +39,31 @@ export class EventController {
   }
 
   @Get(":_id")
-  async findById(@Param("_id", ValidateMongoId) _id: string): Promise<Event> {
-    return await this.eventService.findById(new Types.ObjectId(_id));
+  async findById(@Param("_id", ValidateMongoId) _id: string): Promise<any> {
+    const event = await this.eventService.findById(new Types.ObjectId(_id));
+    return {
+      appId: "tailor",
+      attributes: [
+        {
+          displayType: "string",
+          traitType: "Total Stars",
+          value: event.totalStarts.toString(),
+        },
+        {
+          displayType: "string",
+          traitType: "Total Reviews",
+          value: event.totalReviews.toString(),
+        },
+      ],
+      content: event.title,
+      description: event.description,
+      locale: "en-US",
+      mainContentFocus: "ARTICLE",
+      metadata_id: event._id,
+      name: event.title,
+      tags: ["review3", "tailor"],
+      version: "2.0.0",
+    };
   }
 
   @Patch(":_id")
