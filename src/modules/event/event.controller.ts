5fd4ba389f2bfc4d21d099f4a888abbe820894b3
JYachelini <juliyachedev@gmail.com>
2023-08-17 16:49:35 -0300

    add new controller

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -28,7 +28,6 @@ export class EventController {
     @Body() data: CreateEventDto,
     @UploadedFile() file: Express.Multer.File
   ): Promise<Document<Event>> {
-    console.log("llega");
     return await this.eventService.create(data, file);
   }
 
@@ -38,32 +37,9 @@ export class EventController {
     return await this.eventService.find(page, limit);
   }
 
-  @Get(":_id")
-  async findById(@Param("_id", ValidateMongoId) _id: string): Promise<any> {
-    const event = await this.eventService.findById(new Types.ObjectId(_id));
-    return {
-      appId: "tailor",
-      attributes: [
-        {
-          displayType: "string",
-          traitType: "Total Stars",
-          value: event.totalStarts.toString(),
-        },
-        {
-          displayType: "string",
-          traitType: "Total Reviews",
-          value: event.totalReviews.toString(),
-        },
-      ],
-      content: event.title,
-      description: event.description,
-      locale: "en-US",
-      mainContentFocus: "ARTICLE",
-      metadata_id: event._id,
-      name: event.title,
-      tags: ["review3", "tailor"],
-      version: "2.0.0",
-    };
+  @Get("event2/:_id")
+  async findById(@Param("_id", ValidateMongoId) _id: string): Promise<Event> {
+    return await this.eventService.findById(new Types.ObjectId(_id));
   }
 
   @Patch(":_id")
